4a4d3d6dcd056ac67f09897f1f1bf54d590f6cd8
unknown <JSzwed@JSzwed.DFORCOM.localdomain>
2018-06-08 12:02:17 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # automation-task
+To run the testcase.py Python 3, Selenium, geckodriver and Firefox are required.
--- testcase.py
+++ testcase.py
@@ -4,14 +4,20 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Firefox()
-wait = WebDriverWait(driver, 10)
+wait = WebDriverWait(driver, 30)
 driver.get("https://www.f-secure.com/en/web/about_global/careers/job-openings")
+
 try:
-    if driver.find_element_by_id("cookie-consent").is_displayed():
+    passed = None
+    # Click OK on cookie consent if visible
+    cookieConsent = driver.find_element_by_id("cookie-consent")
+    if cookieConsent.value_of_css_property("z-index") != "-1":
         driver.find_element_by_xpath(
             "//div[@id='cookie-consent']"
             "//ancestor::a[text()='OK']"
             ).click()
+
+    # Choose the city
     cities = (
         "//div[@class='form-group filled']"
         "//span[text()='All cities']"
@@ -27,6 +33,15 @@ try:
         )
     driver.find_element_by_xpath(cities).click()
     driver.find_element_by_xpath(poznan).click()
+    firstJob = (
+        "//div[@id='job-ads']"
+        "//article[1]"
+        )
+    wait.until(
+        EC.visibility_of_element_located((By.XPATH, firstJob))
+        )
+
+    # Check if there is desired job offer
     QAJob = (
         "//div[@id='job-ads']"
         "//h2[text()='Quality Engineer']"
@@ -35,33 +50,49 @@ try:
     wait.until(
         EC.presence_of_element_located((By.XPATH, QAJob))
         )
+
+    # Handle pagination
     pages = driver.find_element_by_xpath(
         "//ul[starts-with(@class, 'pagination')]"
         "//li[last()-1]"
         "//a"
         ).text
     count = int(pages)
+
     for i in range(count):
         if driver.find_element_by_xpath(QAJob).is_displayed():
             driver.find_element_by_xpath(QAJob).click()
             break
         else:
-            nextPage = driver.find_element_by_xpath(
+            nextPage = (
                 "//ul[starts-with(@class, 'pagination')]"
                 "//a[@class='page-link next']"
-                ).click()
-    wait.until(lambda d: 1 != len(driver.window_handles))
+                )
+            wait.until(
+                EC.presence_of_element_located((By.XPATH, nextPage))
+                )
+            driver.find_element_by_xpath(nextPage).click()
+
+    # Assert the job offer page
+    wait.until(lambda driver: 1 != len(driver.window_handles))
     driver.switch_to.window(driver.window_handles[-1])
+
     wait.until(
         EC.presence_of_element_located((By.TAG_NAME, "title"))
         )
     assert "Quality Engineer" in driver.title
+
     wait.until(
         EC.presence_of_element_located((By.TAG_NAME, "body"))
         )
     bodyText = driver.find_element_by_tag_name('body').text
     assert "Quality Engineer wanted" in bodyText
-    pass
+    passed = True
 finally:
     driver.quit()
-    print("Test finished")
+    finalMessage = "Test finished "
+    if passed is True:
+        finalMessage += "successfully!"
+    else:
+        finalMessage += "with an error..."
+    print(finalMessage)
